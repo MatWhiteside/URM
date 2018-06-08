d57e11359eeb1dc566262b9d67f57b5978da797e
Matthew Whiteside <matwhiteside1@gmail.com>
2018-06-08 14:53:41 +0100

    Wrote function to encode instructions so they're easier to process

--- URM.py
+++ URM.py
@@ -2,7 +2,7 @@
 Instructions:
  - Rk := Rk + 1
  - Rk := Rk - 1
- - if B then goto q
+ - if (B) then goto q
  - stop: finished entering instructions
 """
 
@@ -27,10 +27,33 @@ def add_instruction(instruction):
     instructions.append(instruction)
 
 
+"""
+Encodes instructions so they can be executed:
+ - Successor: [s, reg#]
+ - Predecessor: [p, reg#]
+ - Cond. goto: [cond, reg#]
+"""
+def encode_instructions():
+    global instructions
+    for input_num in range(len(instructions)):
+        # Fetch the instruction to encode
+        ins_to_process = instructions[input_num]
+
+        # Encode the instruction
+        if "+" in ins_to_process:   # Successor
+            instructions[input_num] = ["s", ins_to_process[0:ins_to_process.find(" ")]]
+        elif "-" in ins_to_process: # Predecessor
+            instructions[input_num] = ["p", ins_to_process[0:ins_to_process.find(" ")]]
+        elif "goto" in ins_to_process:  # Conditional goto
+            instructions[input_num] = [
+                ins_to_process[ins_to_process.find("(")+1:ins_to_process.find(")")],
+                ins_to_process[-ins_to_process[::-1].find(" "):]
+            ]
+
+
 if __name__ == '__main__':
     # Take the program as an input via the console
     ins = input("Enter instruction: ")
-
     while ins != "stop":
         add_instruction(ins)
         ins = input("Enter instruction: ")
@@ -38,10 +61,12 @@ if __name__ == '__main__':
     parameters = []
     # Take the inputs via the console
     param = input("Enter parameter: ")
-
     while param != "stop":
         parameters.append(param)
         param = input("Enter parameter: ")
 
     # Initialise 100 registers, should be sufficient for smaller URM programs
     init_urm(100, parameters)
+
+    encode_instructions()
+    print(instructions)
