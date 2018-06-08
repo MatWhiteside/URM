9533668bc14b742cee32dd6f8abf14540b6022d6
Matthew Whiteside <matwhiteside1@gmail.com>
2018-06-08 14:25:42 +0100

    Added function to initialise the URM registers with 0's and take inputs that are also loaded into the registers

--- URM.py
+++ URM.py
@@ -7,10 +7,19 @@ Instructions:
 """
 
 
-
 registers = []
 instructions = []
-PC = 0
+pc = 0
+
+
+def init_urm(num_registers, inputs):
+    # Set n registers to 0 where n = num_registers
+    for i in range(0, num_registers):
+        registers.append(0)
+
+    # Load the inputs into the registers
+    for input_num in range(len(inputs)):
+        registers[input_num] = inputs[input_num]
 
 
 def add_instruction(instruction):
@@ -19,10 +28,20 @@ def add_instruction(instruction):
 
 
 if __name__ == '__main__':
+    # Take the program as an input via the console
     ins = input("Enter instruction: ")
 
     while ins != "stop":
         add_instruction(ins)
         ins = input("Enter instruction: ")
 
+    parameters = []
+    # Take the inputs via the console
+    param = input("Enter parameter: ")
 
+    while param != "stop":
+        parameters.append(param)
+        param = input("Enter parameter: ")
+
+    # Initialise 100 registers, should be sufficient for smaller URM programs
+    init_urm(100, parameters)
